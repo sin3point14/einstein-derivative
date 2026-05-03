9cba43179a247b10516a508f64d52cbf3ad204b9
Manas Chaudhary <manaschaudhary2000@gmail.com>
2026-05-04 03:51:08 +0530

    Add stvk example

--- example.py
+++ example.py
@@ -15,6 +15,23 @@ def test_scalar() -> None:
 
     print(y.diff(x[()]))
 
+def test_dot_prod() -> None:
+    y, a, x = Scalar("y"), Vector("a"), Vector("x")
+    i, j = Index("i"), Index("j")
+    y[()] = a[i] * x[i]
+
+    print(y.diff(x[j]))
+
+
+def test_stvk_green_strain() -> None:
+    psi, mu, lambda_by_2 = Scalar("psi"), Scalar("mu"), Scalar("lambda_by_2")
+    E = Matrix("E")
+    i, j, k, l = Index("i"), Index("j"), Index("k"), Index("l")
+
+    psi[()] = mu[()] * E[i, j] * E[i, j] + lambda_by_2[()] * E[i, i] * E[j, j]
+    print(psi.diff(E[k, l]))
 
 test_matmul()
 test_scalar()
+test_dot_prod()
+test_stvk_green_strain()
